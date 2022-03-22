4b380ed312ed439e215e0c0bc94714ee213f8539
Manjumithun <101967918+Manjumithun@users.noreply.github.com>
2022-03-22 17:44:33 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Calculator obj=new Calculator();
+      System.out.println(obj.add(10, 20, 30));
+      System.out.println(obj.sub(50,10));
+      System.out.println(obj.mul(10,10.05));
+      System.out.println(obj.divide(100f, 10f));
+      
+	}
+
+}
